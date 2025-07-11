12efc5e347e37e010b0a33b6d99a45c0234f7f3e
shofiyatunnisa9 <shofiyatunnisa939@gmail.com>
2025-07-11 15:33:49 +0700

    fix: supabase id and role key

--- src/configs/supabaseClient.ts
+++ src/configs/supabaseClient.ts
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const supabase = createClient(
-  "https://cogaheelmgfxrxwxjfmw.supabase.co",
+  "https://llobhwcebmowtsmuobhd.supabase.co",
   process.env.SERVICE_ROLE_KEY as string
 );
